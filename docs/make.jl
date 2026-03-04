63d9b00d7c8337c823e11ad2b2c25fcc3461621e
Ntropic <21343359+Ntropic@users.noreply.github.com>
2026-03-04 12:37:25 +0100

    "Updated for nextcloud push"

--- docs/make.jl
+++ docs/make.jl
@@ -2,15 +2,15 @@ using PulseArchitect
 using Documenter
 
 makedocs(
-    modules = [PulseArchitect],
-    sitename = "PulseArchitect.jl",
-    pages = [
+    modules=[PulseArchitect],
+    sitename="PulseArchitect.jl",
+    pages=[
         "Home" => "index.md",
         "API Reference" => [
             "Pulse Architect" => "api/pulse_architect.md",
-            "Effects" => "api/effects.md",        
-            "Audio Chains" => "api/audio_chains.md",      
-            "Wave Generators" => "api/wave_generators.md",  
+            "Effects" => "api/effects.md",
+            "Audio Chains" => "api/audio_chains.md",
+            "Wave Generators" => "api/wave_generators.md",
         ]
     ]
 )
